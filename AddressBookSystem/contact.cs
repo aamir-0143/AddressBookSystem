4beba2d1d9bb2db09a7b03c88627000da1a1e40c
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-06 02:27:49 +0530

    [Aamir9] Add ; Added EditContacts

--- AddressBookSystem/contact.cs
+++ AddressBookSystem/contact.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations;
+//namespace AddressBook
+//{
+//    public class Contact
+//    {
+//        // get method-returns the value of the variable.
+//        // set method-assigns a value to the variable. 
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+//        public string City { get; set; }
+//        public string State { get; set; }
+//        public string ZipCode { get; set; }
+//        public string PhoneNumber { get; set; }
+//        public string Email { get; set; }
+//    }
+//}
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
